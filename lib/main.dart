f8576a97c72ff327c61d2f5fccbcf2b12c5a3377
KevinBSC97 <kevinsotomayor1@hotmail.com>
2023-09-04 09:52:44 -0500

    datasource y repositorios

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
+import 'package:toktik/infrastructure/datasources/local_video_datasource_impl.dart';
+import 'package:toktik/infrastructure/repositories/video_posts_repository_impl.dart';
 import 'package:toktik/presentation/providers/discover_provider.dart';
 import 'package:toktik/presentation/screens/discover/discover_screen.dart';
 
@@ -12,11 +14,15 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    final videoPostRepository = VideoPostsRepositoryImpl(
+      videosDatasource: LocalVideoDatasource()
+    );
+
     return MultiProvider(
       providers: [
         ChangeNotifierProvider(
           lazy: false, //Esta propiedad ayuda a que cuando se crea la referencia a nuestro DiscoverProvider, se cree la instacia
-          create: (_) => DiscoverProvider()..loadNextPage()
+          create: (_) => DiscoverProvider(videosRepository: videoPostRepository)..loadNextPage()
         ),//los .. son operador de cascada
       ],
       child: MaterialApp(
